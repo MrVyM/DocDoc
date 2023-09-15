c0bd3606d1a7bf005423bf23fe298b9503295719
MrVym <la.regle.du.jeu.ytb@gmail.com>
2023-09-15 09:24:17 +0200

    [SUMMARY] Remove the echo with title

--- summary.sh
+++ summary.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-SRC_FILE=$(ls | grep .md | grep -v $1 | grep -v $2) 
-echo -e "# Wiki Robot\n## Sommaire" > $1
+cd docs/
+SRC_FILE=$(ls | grep .md | grep -v "$1" | grep -v "$2") 
+echo -e "## Sommaire" > "$1"
 
 for file in ${SRC_FILE}
 do 
     echo "Merging the ${file} file"
     filename=$(echo ${file} | cut -d "." -f 1)
     echo -e "- [$filename]($(head -n 1 ${file} | sed 's/# /#/1'))" >> $1
-
 done
