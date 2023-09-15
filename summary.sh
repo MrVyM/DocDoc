6b3240f8d3559e89991278de9bba44c263a8f4f8
MrVym <la.regle.du.jeu.ytb@gmail.com>
2023-09-15 15:41:55 +0200

    [SUMMARY] Update the code to work with ## and ###

--- summary.sh
+++ summary.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+repeat()
+{
+    for i in $(seq 3 $1); do echo -n '\t'; done
+}
+
 cd docs/
 SRC_FILE=$(ls | grep .md | grep -v "$1" | grep -v "$2") 
 echo -e "## Sommaire" > "$1"
@@ -7,6 +12,14 @@ echo -e "## Sommaire" > "$1"
 for file in ${SRC_FILE}
 do 
     echo "Merging the ${file} file"
-    filename=$(echo ${file} | cut -d "." -f 1)
-    echo -e "- [$filename]($(head -n 1 ${file} | sed 's/# /#/1'))" >> $1
+    for topic in $(cat ${file} | grep -i '^[#]' | sed 's/ /-/g' | sed -e 's/\-*$//g')
+    do
+        count=$(echo ${topic} | cut -d '-' -f 1 | wc -m)
+        offset=$(repeat ${count})
+        topic=$(echo ${topic} | cut -d '-' -f 2-)
+        echo -e "${offset}- [${topic}](#${topic,,})" >> $1
+    done
 done
+
+
+
